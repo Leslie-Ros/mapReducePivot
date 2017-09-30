4e44b4e7f4df3cf5fc7fcf7e62c8b372295eecff
Leslie-Ros <aspire-one@hotmail.fr>
2017-09-30 09:49:17 +0200

    Map/Reduce

---
+++ pivotMapReduce.java
@@ -0,0 +1,28 @@
+
+  @Override
+  public void map(IntWritable key, Text value, Context context)
+          throws IOException, InterruptedException {
+            int index = key.get();
+            IntWritable keyMap = new IntWritable();
+            Text word = new Text();
+            StringTokenizer itr = new StringTokenizer(value.toString(),",");
+            while (itr.hasMoreTokens()){
+              word.set(itr.nextToken());
+              keyMap.set(index);
+              context.write(keyMap, word);
+              index =+ 1;
+            }
+          }
+
+@Override
+public void reduce(IntWritable key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException {
+          Text output = new Text();
+          for (Text val : values) {
+            String ligne ='';
+            ligne += val.toString() + ',';
+            ligne = ligne.substring(0, ligne.length() - 1)
+          }
+          output.set(ligne);
+          context.write(key,output);
+        }
